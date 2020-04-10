1245b81eceb4c66c7c7db0b0b67a36e6477e8ffe
Mateus <63373687+MateSilver@users.noreply.github.com>
2020-04-09 21:45:41 -0300

    mais organização e otimização

--- ordenaçoes_uteis/ordenação_bubble_sort.c
+++ ordenaçoes_uteis/ordenação_bubble_sort.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-void bubbleSort(int* v, int n)
-{
-    if (n < 1)
-	{
+void bubbleSort(int* v, int n){
+	int i;
+    if (n < 1){
 		return;
 	}
 
-    for (int i=0; i<n; i++)//passa o maior pra frente
-	{
-        if (v[i] > v[i+1])
-		{
+    for (int i=0; i<n; i++){
+        if (v[i] > v[i+1]){
 			v[i] += v[i+1];
 			v[i+1] = v[i] - v[i+1];
 			v[i] -= v[i+1];
@@ -18,22 +15,18 @@ void bubbleSort(int* v, int n)
 	}
     return(bubbleSort(v, n-1));
 }
-int main()
-{
+int main(){
 	int n,i;
 	scanf("%d", &n);
 
 	int v[n];
-	for(i=0; i<n; i++)
-	{
+	for(i=0; i<n; i++){
 		scanf("%d", &v[i]);
 	}
-	getchar();
+
 	bubbleSort(v,n);
-	for(i=0; i<n; i++)
-    {
+	for(i=0; i<n; i++){
         printf("%d\n", v[i]);
-    }
-    getchar();
-    return(0);
+	}
 }
+
