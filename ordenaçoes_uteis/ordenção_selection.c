48818f8c315db6b5978fb9035f05b08f71b33aeb
Mateus <63373687+MateSilver@users.noreply.github.com>
2020-04-09 22:02:31 -0300

    selection_sort
    maior organização e optimização nessa versao

--- ordenaçoes_uteis/ordenção_selection.c
+++ ordenaçoes_uteis/ordenção_selection.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 
-void swap_quant(int a,int b)
-{
+void swap_quant(int a,int b){
     a += b;
     b = a-b;
     a -= b;
 }
-void selection_sort(int* v, int n)
-{
+void selection_sort(int* v, int n){
 	int i, j, min, aux;
-	for (i = 0; i < (n-1); i++)//compara com os seguintes
-	{
+	for (i = 0; i < (n-1); i++){
 		min = i;
-		for (j = (i+1); j < n; j++)
-		{
+		for (j = (i+1); j < n; j++){
 			if(v[j] < v[min])
 				min = j;
 		}
-		if (v[i] != v[min])//se achado um menor
-		{
+		if (v[i] != v[min]){
             v[i] += v[min];
             v[min] = v[i]-v[min];
             v[i] -= v[min];
@@ -26,23 +21,19 @@ void selection_sort(int* v, int n)
 
 	}
 }
-int main()
-{
+int main(){
 	int n, i;
 
 	scanf("%d", &n);
 	int v[n];
 
-	for(i=0; i<n; i++)
-	{
+	for(i=0; i<n; i++){
 		scanf("%d", &v[i]);
 	}
 	getchar();
 	selection_sort(v,n);
-	for(i=0; i<n; i++)
-    {
+	for(i=0; i<n; i++){
         printf("%d\n", v[i]);
     }
-    getchar();
-    return(0);
 }
+
